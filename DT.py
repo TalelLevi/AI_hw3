8996af0134794054aa0cdec14f95e8137eb1308c
Tal-el levi <kilek1751@gmail.com>
2020-06-20 15:06:48 +0300

    working basic classifer ID3

--- DT.py
+++ DT.py
@@ -4,21 +4,31 @@ from ID3 import ID3
 
 
 
-df = pd.read_csv('test.csv')
+df = pd.read_csv('train.csv')
 
 classifer = ID3(0)
 classifer.train(df)
 
+df2 = pd.read_csv('test.csv')
 
 for i in range(len(df)):
     original_diagnosis = df.loc[i, 'diagnosis']
-    example = df.drop('diagnosis', axis=1).loc[i, :]
+    example = df.loc[i, :]
     our_answer = classifer.classify(example)
-    print(f'{our_answer} == {original_diagnosis} count is {i}')
+    # print(f'{our_answer} == {original_diagnosis} count is {i}')
     assert(original_diagnosis == our_answer)
 
+count = 0
+for i in range(len(df2)):
+    original_diagnosis = df2.loc[i, 'diagnosis']
+    example = df2.loc[i, :]
+    our_answer = round(classifer.classify(example))
+    if our_answer != original_diagnosis:
+        count += 1
+        print(f'{our_answer} == {original_diagnosis} count is {i}')
 
 
+print(f'success rate is {(1-count/len(df2))*100}')
 
 
 
--- ID3.py
+++ ID3.py
@@ -98,21 +98,3 @@ class ID3:
                     best_split_feature = feature
                     best_split_value = value
         return best_split_feature, best_split_value
-#
-# ID3 (Examples, Target_Attribute, Attributes)
-#     Create a root node for the tree
-#     If all examples are positive, Return the single-node tree Root, with label = +.
-#     If all examples are negative, Return the single-node tree Root, with label = -.
-#     If number of predicting attributes is empty, then Return the single node tree Root,
-#     with label = most common value of the target attribute in the examples.
-#     Otherwise Begin
-#         A ← The Attribute that best classifies examples.
-#         Decision Tree attribute for Root = A.
-#         For each possible value, vi, of A,
-#             Add a new tree branch below Root, corresponding to the test A = vi.
-#             Let Examples(vi) be the subset of examples that have the value vi for A
-#             If Examples(vi) is empty
-#                 Then below this new branch add a leaf node with label = most common target value in the examples
-#             Else below this new branch add the subtree ID3 (Examples(vi), Target_Attribute, Attributes – {A})
-#     End
-#     Return Root
